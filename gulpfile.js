6562f125578c146d493849a692730812b6ba8559
Emanuel Cordova <emanuelcord@gmail.com>
2023-10-03 10:38:25 -0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
+const { src, dest, watch} = require("gulp");
+const sass = require("gulp-sass")(require('sassnpm run '));
 
 function css(done) {
-
-
+    src("src/scss/app.scss")//Identificar el Archivo
+     .pipe( sass())//Compilarlo
+     .pipe( dest("build/css"));//Almacenarlo en el disco duro
     done();
 }
+
+function dev(done) {
+    watch("src/scss/app.scss", css)
+
+    done()
+}
+
+exports.css = css;
+exports.dev = dev;
